196dd4f108998ef9225b544c418577cc3d3d049b
snail15 <jungs@uchicago.edu>
2022-09-11 23:49:40 -0700

    finish modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,20 +6,22 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div>
         <span>Total Amount</span>
         <span>35.55</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button onClick={props.onClose} className={classes['button--alt']}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import classes from './Modal.module.css';
-import { ReactDOM } from 'react';
+import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
-  <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
-  <div className={classes.modal}>
-    <div className={classes.content}>{props.children}</div>
-  </div>;
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
 };
 
-const portalElement = document.getElementById('overlay');
+const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)};
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      ;
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
